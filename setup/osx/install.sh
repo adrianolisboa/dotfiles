169da8c432b5135ec0c8fabd365271aa4b69b1ff
Adriano Lisboa <adrianolic@gmail.com>
2018-05-29 21:13:29 -0300

    Fix my Brewfile dependencies

--- setup/osx/install.sh
+++ setup/osx/install.sh
@@ -3,3 +3,9 @@
 # Define OSX KeyBoard velocity
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+
+# Install Homebrew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Download dependencies
+brew bundle
