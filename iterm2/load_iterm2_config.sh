6a1e5c357be7ff5b2c8d600c3a14467b21617c9b
Adriano Lisboa <adrianolic@gmail.com>
2018-07-04 19:30:58 -0300

    Set prompt on quit to false at iterm2 config

--- iterm2/load_iterm2_config.sh
+++ iterm2/load_iterm2_config.sh
@@ -8,3 +8,5 @@ defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string $directory
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
