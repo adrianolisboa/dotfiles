4ccaec8e4f370bb8514797ce683f85a2d5d77f28
Adriano Lisboa <adrianolic@gmail.com>
2018-07-05 09:31:35 -0300

    Add gitconfig dotfile

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -f ~/.bashrc ~/.inputrc 
+rm -f ~/.bashrc ~/.inputrc ~/.gitconfig
 
 ln -s .env-setup/dotfiles/bashrc ~/.bashrc
 ln -s .env-setup/dotfiles/inputrc ~/.inputrc
+ln -s .env-setup/dotfiles/gitconfig ~/.gitconfig
