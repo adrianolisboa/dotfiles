8438f51dee00c8a04ae79a6d3f7355fea57528f0
Adriano Lisboa <adrianolic@gmail.com>
2018-11-20 08:55:01 -0200

    Add gitexcludes file

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm -f ~/.bashrc ~/.inputrc ~/.gitconfig
+rm -f ~/.bashrc ~/.inputrc ~/.gitconfig ~/.gitexcludes
 
 ln -s .env-setup/dotfiles/bashrc ~/.bashrc
 ln -s .env-setup/dotfiles/inputrc ~/.inputrc
 ln -s .env-setup/dotfiles/gitconfig ~/.gitconfig
+ln -s .env-setup/dotfiles/gitexcludes ~/.gitexcludes
