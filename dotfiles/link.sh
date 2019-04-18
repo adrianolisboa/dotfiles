b8d465c43175c31d76fd19726b66660ebecf76d1
Adriano Lisboa <adrianolic@gmail.com>
2019-04-18 09:20:39 -0300

    Remove tmux conf first when linking again

--- dotfiles/link.sh
+++ dotfiles/link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f ~/.bashrc ~/.inputrc ~/.gitconfig ~/.gitexcludes
+rm -f ~/.bashrc ~/.inputrc ~/.gitconfig ~/.gitexcludes ~/.tmux.conf
 
 ln -s .env-setup/dotfiles/bashrc ~/.bashrc
 ln -s .env-setup/dotfiles/inputrc ~/.inputrc
